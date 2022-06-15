ea6d47196015472f7f7b400657f61078ad1b78fd
Ignacio Aristo <ignacioaristo@gmail.com>
2022-06-15 00:55:13 -0300

    Modal informativo cuando se adivina la palabra

--- app.js
+++ app.js
@@ -9,57 +9,106 @@ window.onload = function(){
   var allInputsfourthFormInput = document.getElementsByClassName("fourthFormInput");
   var buttonFormFive = document.getElementById("buttonFormFive");
   var allInputsfifthFormInput = document.getElementsByClassName("fifthFormInput");
+  var tituloModal = document.getElementById("modal-title");
   var respuesta = 'piano';
   var respuestaSplit = respuesta.split('')
+  let contador = 0;
+
+  function openModal() {
+    modalSection.style.display = "flex";
+  }
 
   buttonFormOne.onclick = function (e) {
     e.preventDefault();
     Array.from(allInputsFirstForm).forEach(function(input, index, array){
       input.setAttribute("disabled", "");
-      if (respuestaSplit[index] === input.value) return input.classList.add('acierto');
+      if (respuestaSplit[index] === input.value) {
+        contador++
+        return input.classList.add("acierto");
+      };
       if (!respuestaSplit.includes(input.value)) return input.classList.add('noExiste');
       if (respuestaSplit.includes(input.value) && respuestaSplit[index] !== input.value) return input.classList.add("lugarEquivocado");
     })
+    if (contador === 5) {
+      tituloModal.innerText = "GANASTE";
+      return openModal()
+    };
   };
 
   buttonFormTwo.onclick = function (e) {
     e.preventDefault();
     Array.from(allInputsSecondForm).forEach(function (input, index, array) {
       input.setAttribute("disabled", "");
-      if (respuestaSplit[index] === input.value) return input.classList.add("acierto");
+      if (respuestaSplit[index] === input.value) {
+        contador++;
+        return input.classList.add("acierto");
+      }
       if (!respuestaSplit.includes(input.value)) return input.classList.add("noExiste");
       if (respuestaSplit.includes(input.value) && respuestaSplit[index] !== input.value) return input.classList.add("lugarEquivocado");
     });
+    if (contador === 5) return console.log("GANASTE!");
   };
 
   buttonFormThree.onclick = function (e) {
     e.preventDefault();
     Array.from(allInputsthirdFormInput).forEach(function (input, index, array) {
       input.setAttribute("disabled", "");
-      if (respuestaSplit[index] === input.value) return input.classList.add("acierto");
+      if (respuestaSplit[index] === input.value) {
+        contador++
+        return input.classList.add("acierto");
+      }
       if (!respuestaSplit.includes(input.value)) return input.classList.add("noExiste");
       if (respuestaSplit.includes(input.value) && respuestaSplit[index] !== input.value) return input.classList.add("lugarEquivocado");
     });
+    if (contador === 5) return console.log("GANASTE!");
   };
 
   buttonFormFour.onclick = function (e) {
     e.preventDefault();
-      input.setAttribute("disabled", "");
     Array.from(allInputsfourthFormInput).forEach(function (input, index, array) {
       input.setAttribute("disabled", "");
-      if (respuestaSplit[index] === input.value) return input.classList.add("acierto");
+      if (respuestaSplit[index] === input.value) {
+        contador++
+        return input.classList.add("acierto");
+      }
       if (!respuestaSplit.includes(input.value)) return input.classList.add("noExiste");
       if (respuestaSplit.includes(input.value) && respuestaSplit[index] !== input.value) return input.classList.add("lugarEquivocado");
     });
+    if (contador === 5) return console.log("GANASTE!");
   };
 
   buttonFormFive.onclick = function (e) {
     e.preventDefault();
     Array.from(allInputsfifthFormInput).forEach(function (input, index, array) {
       input.setAttribute("disabled", "");
-      if (respuestaSplit[index] === input.value) return input.classList.add("acierto");
+      if (respuestaSplit[index] === input.value) {
+        contador++
+        return input.classList.add("acierto");
+      }
       if (!respuestaSplit.includes(input.value)) return input.classList.add("noExiste");
       if (respuestaSplit.includes(input.value) && respuestaSplit[index] !== input.value) return input.classList.add("lugarEquivocado");
     });
+    if (contador === 5) return console.log("GANASTE!");
   };
+
+ // MODAL SETTINGS
+
+  var modalSection = document.getElementById('modal-section');
+
+  function openModal (){
+      modalSection.style.display = 'flex';
+  }
+
+  function closeModal (e){
+      if(e.target == modalSection){
+          modalSection.style.display = 'none';
+      }
+  }
+  window.addEventListener('click', closeModal);
+
+  var modalCloseBtn = document.getElementById('modal-close-button');
+  modalCloseBtn.onclick = function (){
+      modalSection.style.display = 'none';
+  }
 }
+
